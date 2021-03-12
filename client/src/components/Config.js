69a6ffa15c98406e423ebd867c05738fd61ed8c1
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-03-12 18:34:59 +0900

    First Commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "14bca6c7c6d1fc9be81f5c138aa5b19f";
+
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
