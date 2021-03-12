b26545dbd6daad232670aa1d8528d350ece6a19e
minidevhs <78305298+minidevhs@users.noreply.github.com>
2021-03-13 02:27:58 +0900

    #11 Favorite Button

---
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
+function Favorite(props) {
+  const movieId = props.movieId;
+  const userFrom = props.userFrom;
+  const movieTitle = props.movieInfo.title;
+  const moviePost = props.movieInfo.backdrop_path;
+  const movieRunTime = props.movieInfo.runtime;
+
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
+
+  useEffect(() => {
+    let variables = {
+      userFrom,
+      movieId,
+    };
+
+    Axios.post("/api/favorite/favoriteNumber", variables).then((response) => {
+      if (response.data.success) {
+        setFavoriteNumber(response.data.FavoriteNumber);
+      } else {
+        alert("숫자 정보를 가져오는데 실패 했습니다.");
+      }
+    });
+
+    Axios.post("/api/favorite/favorited", variables).then((response) => {
+      if (response.data.success) {
+        setFavorited(response.data.Favorited);
+      } else {
+        alert("정보를 가져오는데 실패 했습니다.");
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <button>
+        {Favorited ? " Not Favorite" : "Add to Favorite "} {FavoriteNumber}
+      </button>
+    </div>
+  );
+}
+
+export default Favorite;
---
+++ server/routes/favorite.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const { Favorite } = require("../models/Favorite");
+
+router.post("/favoriteNumber", (req, res) => {
+  // mongoDB에서 favorite 숫자를 가져오기
+  Favorite.find({ movieId: req.body.movieId }).exec((err, info) => {
+    if (err) return res.status(400).send(err);
+    // 그다음에 프론트에 다시 숫자 정보를 보내주기
+    res.status(200).json({ success: true, FavoriteNumber: info.length });
+  });
+});
+
+router.post("/favorited", (req, res) => {
+  // 내가 이 영화를 Favorite 리스트에 넣었는지 정보를 DB에서 가져오기
+
+  Favorite.find({
+    movieId: req.body.movieId,
+    userFrom: req.body.userFrom,
+  }).exec((err, info) => {
+    if (err) return res.status(400).send(err);
+    // 그다음에 프론트에 다시 숫자 정보를 보내주기
+
+    let result = false;
+    if (info.length !== 0) {
+      result = true;
+    }
+
+    res.status(200).json({ success: true, Favorited: result });
+  });
+});
+
+module.exports = router;
